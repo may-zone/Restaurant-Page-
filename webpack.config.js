eb1c30513dcb1faf699c747a2aebf51bad859163
ProudMay <mehdi79nekouei@gmail.com>
2025-11-12 11:46:03 +0330

    feat [  install and config webpack dev server ]

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-
-module.exports ={
-    mode:"development",
-    entry:"./src/script.js",
-    output:{
-        filename : "main.js",
-        path : path.resolve(__dirname,"dist"),
-        clean:true,
-    },
-    plugins :[
-        new HtmlWebPackPlugin({
-            template :"./src/index.html",
-        }),
+module.exports = {
+  mode: "development",
+  entry: "./src/script.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.html"],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    module : {
-        rules: [
-            {
-                test : /\.css$/i,
-                use : ["style-loader", "css-loader"]
-            },
-        ],
-    },
+  },
 };
