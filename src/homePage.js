1f8524e70f0295d8ab184bc465eaf8a6b7afe36b
ProudMay <mehdi79nekouei@gmail.com>
2025-11-12 20:57:19 +0330

    fix [fix  duplicate items when click on home btn ]

--- src/homePage.js
+++ src/homePage.js
@@ -1,32 +1,20 @@
 import resImg from "./images/res.jpg";
 
- export function homePage () {
-const header = document.createElement('header');
-header.innerHTML = `
-<nav>
-<button id="home"> Home </button>
-<button id="menu"> Menu </button>
-<button id="about"> About us </button>
-</nav>
+export function homePage() {
+  const content = document.getElementById('content');
+  content.innerHTML = ""; 
 
-`
-const content = document.createElement('div');
-content.id='content';
-document.body.appendChild(content);
+  const hometittle = document.createElement('h2');
+  hometittle.textContent = "this is a Tittle";
 
-const contentDiv = document.getElementById('content');
-const hometittle = document.createElement('h2');
-hometittle.textContent = "this is a Tittle";
+  const contentImg = document.createElement('img');
+  contentImg.src = resImg;
+  contentImg.alt = "Restaurant image of benjamin cheng in unsplash.com";
 
-const contentImg = document.createElement('img');
-contentImg.src = resImg;
-contentImg.alt = "Restaurant image of benjamin cheng in unsplash.com";
+  const imgBullshits = document.createElement('p');
+  imgBullshits.textContent = "here is some data about restaurant ";
 
-const imgBullshits = document.createElement('p');
-imgBullshits.textContent = "here is some data about restaurant ";
-
-document.body.prepend(header);
-contentDiv.appendChild(hometittle);
-contentDiv.appendChild(contentImg);
-content.appendChild(imgBullshits);
+  content.appendChild(hometittle);
+  content.appendChild(contentImg);
+  content.appendChild(imgBullshits);
 }
--- src/script.js
+++ src/script.js
@@ -4,6 +4,20 @@ import { loadMenu } from "./menu.js";
 
 
 alert("salam ziba !");
+const header = document.createElement('header');
+header.innerHTML = `
+  <nav>
+    <button id="home">Home</button>
+    <button id="menu">Menu</button>
+    <button id="about">About us</button>
+  </nav>
+`;
+document.body.prepend(header);
+
+const content = document.createElement('div');
+content.id = 'content';
+document.body.appendChild(content);
+
 homePage();
 
 document.getElementById('menu').addEventListener('click',loadMenu);
